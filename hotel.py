06283bb1d8230f00d2312fef07f8f6a2d3a38b9a
perezchris3599 <kweeziperez712@gmail.com>
2023-03-18 20:59:43 +0300

    logo

--- hotel.py
+++ hotel.py
@@ -7,13 +7,23 @@ class HotelManagementSystem:
         self.root=root
         self.root.title("Hotel Management System")
         self.root.geometry("1550x800+0+0")
-
+      
+        ####image#####
         img1=Image.open(r"images/img1.jpg")
         img1=img1.resize((1550, 140),Image.ANTIALIAS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
         Ibling=Label(self.root,image=self.photoimg1,bd=4,relief=RIDGE)
         Ibling.place(x=0,y=0, width=1550,height=140)
+
+        ####logo####
+        img2=Image.open(r"images/logo.jpg")
+        img2=img2.resize((230, 140),Image.ANTIALIAS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        Ibling=Label(self.root,image=self.photoimg2,bd=4,relief=RIDGE)
+        Ibling.place(x=0,y=0, width=230,height=140)
+
 
 
 

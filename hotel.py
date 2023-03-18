07d7b81b86392ac493c57d8759e195c4f9595339
perezchris3599 <kweeziperez712@gmail.com>
2023-03-18 20:50:55 +0300

    image added

--- hotel.py
+++ hotel.py
@@ -1,10 +1,22 @@
 from tkinter import*
+from PIL import ImageTk, Image
+
 
 class HotelManagementSystem:
     def __init__(self,root):
         self.root=root
         self.root.title("Hotel Management System")
         self.root.geometry("1550x800+0+0")
+
+        img1=Image.open(r"images/img1.jpg")
+        img1=img1.resize((1550, 140),Image.ANTIALIAS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        Ibling=Label(self.root,image=self.photoimg1,bd=4,relief=RIDGE)
+        Ibling.place(x=0,y=0, width=1550,height=140)
+
+
+
 
 if __name__=="__main__":
     root=Tk()

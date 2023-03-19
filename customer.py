6c4256cb3a28d20e835bf71b755b64d289630090
perezchris3599 <kweeziperez712@gmail.com>
2023-03-20 01:03:22 +0300

    customer details mini window

--- customer.py
+++ customer.py
@@ -10,16 +10,19 @@ class Cust_Win:
 
         ###title####
         lbl_title=Label(self.root,text="ADD CUSTOMER DETAILS",font=("Times New Roman",18,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
-        lbl_title.place(x=0,y=140,width=1295,height=50)
+        lbl_title.place(x=0,y=0,width=1295,height=50)
 
         ####logo####
         img2=Image.open(r"images/logo.jpg")
-        img2=img2.resize((100, 50),Image.ANTIALIAS)
+        img2=img2.resize((100, 40),Image.ANTIALIAS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         Ibling=Label(self.root,image=self.photoimg2,bd=0,relief=RIDGE)
-        Ibling.place(x=5,y=2, width=100,height=50)
+        Ibling.place(x=5,y=2, width=100,height=40)
 
+        ###labelframe####
+        labelframeleft=LabelFrame(self.root,bd=2,relief=RIDGE,text="Customer Details",font=("times new roman",12,"bold"),padx=2,)
+        labelframeleft.place(x=5,y=50,width=425,height=490)
 
 
 if __name__ == "__main__":

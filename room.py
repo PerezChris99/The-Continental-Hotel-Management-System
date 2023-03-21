4cd8396d4db49978900a655f9c68ddac6396cab6
perezchris3599 <kweeziperez712@gmail.com>
2023-03-21 16:58:33 +0300

    show data table

--- room.py
+++ room.py
@@ -32,8 +32,12 @@ class Roombooking:
         lbl_cust_contact=Label(labelframeleft,text="Customer Contact",font=("arial",12,"bold"),padx=2,pady=6)
         lbl_cust_contact.grid(row=0,column=0,sticky=W)
 
-        entry_contact=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29,state="readonly")
-        entry_contact.grid(row=0,column=1)
+        entry_contact=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=20)
+        entry_contact.grid(row=0,column=1,sticky=W)
+
+        #Fetch data button
+        btnFetchData=Button(labelframeleft,text="Fetch Data",font=("arial",8,"bold"),bg="black",fg="gold",width=10)
+        btnFetchData.place(x=347,y=4)
 
         #check in date
         check_in_date=Label(labelframeleft,font=("arial",12,"bold"),text="Check_In Date:",padx=2,pady=6)
@@ -91,7 +95,90 @@ class Roombooking:
         txtIdNumber=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
         txtIdNumber.grid(row=9,column=1)
 
+        ##bill button
+        btnBill=Button(labelframeleft,text="Bill",font=("arial",11,"bold"),bg="black",fg="gold",width=10)
+        btnBill.grid(row=10,column=0,padx=1,sticky=W)
 
+        ###btns####
+        btn_frame=Frame(labelframeleft,bd=2,relief=RIDGE)
+        btn_frame.place(x=0,y=400,width=412,height=40)
+
+        btnAdd=Button(btn_frame,text="Add",font=("arial",12,"bold"),bg="black",fg="gold",width=8)
+        btnAdd.grid(row=0,column=0,padx=1)
+
+        btnUpdate=Button(btn_frame,text="Update",font=("arial",12,"bold"),bg="black",fg="gold",width=8)
+        btnUpdate.grid(row=0,column=1,padx=1)
+
+        btnDelete=Button(btn_frame,text="Delete",font=("arial",12,"bold"),bg="black",fg="gold",width=8)
+        btnDelete.grid(row=0,column=2,padx=1)
+
+        btnReset=Button(btn_frame,text="Reset",font=("arial",12,"bold"),bg="black",fg="gold",width=8)
+        btnReset.grid(row=0,column=3,padx=1)
+
+        ###rightside image##
+        img3=Image.open(r"images/img1.jpg")
+        img3=img3.resize((520,300),Image.ANTIALIAS)
+        self.photoimg3=ImageTk.PhotoImage(img3)
+        lbling=Label(self.root,image=self.photoimg3,bd=0,relief=RIDGE)
+        lbling.place(x=760,y=55,width=520,height=200)
+
+
+        #===table frame===
+        Table_Frame=LabelFrame(self.root,bd=2,relief=RIDGE,text="View Details And Search System",font=("arial",12,"bold"),padx=2)
+        Table_Frame.place(x=435,y=280,width=860,height=490)
+
+        lblSearchBy=Label(Table_Frame,font=("arial",12,"bold"),text="Search By:",bg="red",fg="white")
+        lblSearchBy.grid(row=0,column=0,sticky=W)
+
+        self.search_var=StringVar()
+        combo_Search=ttk.Combobox(Table_Frame,textvariable=self.search_var,font=("arial",12,"bold"),width=24,state="randomly")
+        combo_Search["value"]=("Contact","Room")
+        combo_Search.current(0)
+        combo_Search.grid(row=0,column=1,padx=2)
+
+        self.txt_search=StringVar()
+        txtSearch=ttk.Entry(Table_Frame,textvariable=self.txt_search,font=("arial",13,"bold"),width=24)
+        txtSearch.grid(row=0,column=2,padx=2)
+
+        btnSearch=Button(Table_Frame,text="Search",font=("arial",11,"bold"),bg="black",fg="gold",width=10)
+        btnSearch.grid(row=0,column=3,padx=1)
+
+        btnShowAll=Button(Table_Frame,text="Show All",font=("arial",11,"bold"),bg="black",fg="gold",width=10)
+        btnShowAll.grid(row=0,column=4,padx=1)
+
+        #===show data table====#
+        details_table=Frame(Table_Frame,bd=2,relief=RIDGE)
+        details_table.place(x=0,y=50,width=860,height=180)
+
+        scroll_x=ttk.Scrollbar(details_table,orient=HORIZONTAL)
+        scroll_y=ttk.Scrollbar(details_table,orient=VERTICAL)
+
+        self.Cust_Details_Table=ttk.Treeview(details_table,column=("contact","checkin","checkout","roomtype","roomavailable","meal","noOfdays"),xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+
+        scroll_x.config(command=self.Cust_Details_Table.xview)
+        scroll_y.config(command=self.Cust_Details_Table.yview)
+
+        self.Cust_Details_Table.heading("contact",text="Contact")
+        self.Cust_Details_Table.heading("checkin",text="CheckIn")
+        self.Cust_Details_Table.heading("checkout",text="CheckOut")
+        self.Cust_Details_Table.heading("roomtype",text="Room Type")
+        self.Cust_Details_Table.heading("roomavailable",text="Room No")
+        self.Cust_Details_Table.heading("meal",text="Meal")
+        self.Cust_Details_Table.heading("noOfdays",text="NoOfDays")
+
+        self.Cust_Details_Table["show"]="headings"
+
+        self.Cust_Details_Table.column("contact",width=100)
+        self.Cust_Details_Table.column("checkin",width=100)
+        self.Cust_Details_Table.column("checkout",width=100)
+        self.Cust_Details_Table.column("roomtype",width=100)
+        self.Cust_Details_Table.column("roomavailable",width=100)
+        self.Cust_Details_Table.column("meal",width=100)
+        self.Cust_Details_Table.column("noOfdays",width=100)
+        
+        self.Cust_Details_Table.pack(fill=BOTH,expand=1)
 
 if __name__ == "__main__":
     root=Tk()
